05cb6a1c3639165cfb1c97f8eeeb843d9ddd2c8a
Alex <alex.grigorenko2942@gmail.com>
2019-06-30 17:59:16 +0300

    add answer controller test for new and create action

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'find nedeed question for creating new answer' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'find nedeed question for creating answer' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'saves new answer to database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to question view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid_answer) } }.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid_answer) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
