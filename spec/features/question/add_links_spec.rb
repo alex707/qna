cfef7c788ebe54f139e14c1dad468931b2a1f6ca
alex.grigorenko2942@gmail.com <alex.grigorenko2942@gmail.com>
2019-09-17 08:29:52 +0300

    add multipy links to question

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -7,19 +7,31 @@ feature 'User can add links to question', %{
 } do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/alex707/d6a7726c9132942cf755aa8e6fb52bfb' }
+  given(:gist_url_ya) { 'https://ya.ru' }
 
-  scenario 'User adds link when asks question' do
+  before do
     sign_in(user)
     visit new_question_path
 
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'Test text'
+  end
 
+  scenario 'User adds links when asks question', js: true do
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
+
+    click_on 'add link for question'
+    all '.nested-fields', count: 2
+
+    within all('.nested-fields').last do
+      fill_in 'Link name', with: 'My gist ya'
+      fill_in 'Url', with: gist_url_ya
+    end
 
     click_on 'Ask'
 
     expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'My gist ya', href: gist_url_ya
   end
 end
