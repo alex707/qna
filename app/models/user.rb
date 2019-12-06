59b2bd9886fd0e104071c702035f8c940c2cb2f9
alex707 <alex.grigorenko2942@gmail.com>
2019-12-06 20:15:40 +0300

    screencast 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,29 @@ class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :awards
+  has_many :authorizations
 
   def author?(resource)
     id == resource.user_id
   end
 
-  def self.find_or_oauth(auth)
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
 
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.create_authorization(auth)
+    end
+    user
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
