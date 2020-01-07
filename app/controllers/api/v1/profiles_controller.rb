d64e697f95bbfd3b523bf2cb715e212439ab6e16
alex707 <alex.grigorenko2942@gmail.com>
2020-01-07 20:19:18 +0300

    profiles list without Me

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,6 +1,11 @@
 module Api
   module V1
     class ProfilesController < BaseController
+      def index
+        @users = User.all_except(current_resource_owner)
+        render json: @users
+      end
+
       def me
         render json: current_resource_owner
       end
