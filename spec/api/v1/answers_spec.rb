0152fcc2568fdf6f8f8d3d57211a01d8d1baa67f
alex707 <alex.grigorenko2942@gmail.com>
2020-01-18 20:59:58 +0300

    answers CUD

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 describe 'Answers API', type: :request do
-  let(:headers) do
-    { 'CONTENT_TYPE' => 'application/json',
-      'ACCEPT' => 'application/json' }
-  end
+  let(:headers) { { 'ACCEPT' => 'application/json' } }
 
   describe 'GET api/v1/answers/:id' do
     let!(:answer) { create(:answer, :with_files) }
@@ -82,6 +79,212 @@ describe 'Answers API', type: :request do
             expect(link_response[attr]).to eq link.send(attr).as_json
           end
         end
+      end
+    end
+  end
+
+  describe 'POST api/v1/questions/:question_id/answers' do
+    let(:user) { create(:user) }
+    let!(:question) { create(:question) }
+
+    it_behaves_like 'API Authorizable' do
+      let(:api_path) { "/api/v1/questions/#{question.id}/answers" }
+      let(:method) { :post }
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+
+      context 'with valid attributes' do
+        let(:answer_attributes) { attributes_for(:answer, user: user.id) }
+        let(:create_response) { json['answer'] }
+
+        before do
+          post "/api/v1/questions/#{question.id}/answers",
+               params: { access_token: access_token.token,
+                         answer: answer_attributes },
+               headers: headers
+        end
+
+        it 'returns 200 status' do
+          expect(response).to be_successful
+        end
+
+        it 'returns all public fields' do
+          %w[id created_at updated_at].each do |attr|
+            expect(create_response[attr].to_s).to_not be_empty
+          end
+
+          answer = Answer.find(create_response['id'])
+
+          %w[body].each do |attr|
+            expect(create_response[attr]).to eq answer[attr.to_sym].as_json
+            expect(create_response[attr]).to eq answer_attributes[attr.to_sym].as_json
+          end
+        end
+
+        it 'contains user object' do
+          answer = Answer.find(create_response['id'])
+
+          expect(create_response['user']['id']).to eq answer.user.id
+          expect(create_response['user']['id']).to eq answer_attributes[:user]
+        end
+      end
+
+      context 'with invalid attributes' do
+        let(:answer_attributes) { attributes_for(:answer, :invalid) }
+
+        before do
+          post "/api/v1/questions/#{question.id}/answers",
+               params: { access_token: access_token.token,
+                         answer: answer_attributes },
+               headers: headers
+        end
+
+        it 'returns unprocessable_entity status' do
+          expect(response.status).to eq 422
+        end
+
+        it 'returns errors list' do
+          expect(json.keys).to be_any
+
+          json.keys.each do |key|
+            expect(key.to_sym.in?(answer_attributes.keys)).to be true
+          end
+        end
+      end
+    end
+  end
+
+  describe 'PATCH api/v1/answers/:id' do
+    let(:user) { create(:user) }
+    let!(:question) { create(:question) }
+    let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+    let!(:answer) { create(:answer, question: question, user: user) }
+
+    it_behaves_like 'API Authorizable' do
+      let(:api_path) { "/api/v1/answers/#{answer.id}" }
+      let(:method) { :patch }
+    end
+
+    context 'as owner of resource' do
+      context 'with valid attributes' do
+        let(:answer_attributes) { attributes_for(:answer, user: user) }
+        let(:update_response) { json['answer'] }
+
+        before do
+          patch "/api/v1/answers/#{answer.id}",
+                params: { access_token: access_token.token,
+                          answer: answer_attributes },
+                headers: headers
+        end
+
+        it 'returns 200 status' do
+          expect(response).to be_successful
+        end
+
+        it 'returns all public fields' do
+          %w[id created_at updated_at].each do |attr|
+            expect(update_response[attr].to_s).to_not be_empty
+          end
+
+          answer = Answer.find(update_response['id'])
+
+          %w[body].each do |attr|
+            expect(update_response[attr]).to eq answer[attr.to_sym].as_json
+            expect(update_response[attr]).to eq answer_attributes[attr.to_sym].as_json
+          end
+        end
+
+        it 'contains user object' do
+          answer = Answer.find(update_response['id'])
+
+          expect(update_response['user']['id']).to eq answer.user.id
+          expect(update_response['user']['id']).to eq answer_attributes[:user].id
+        end
+      end
+
+      context 'with invalid attributes' do
+        let(:answer_attributes) { attributes_for(:answer, :invalid) }
+
+        before do
+          patch "/api/v1/answers/#{answer.id}",
+                params: { access_token: access_token.token,
+                          answer: answer_attributes },
+                headers: headers
+        end
+
+        it 'returns unprocessable_entity status' do
+          expect(response.status).to eq 422
+        end
+
+        it 'returns errors list' do
+          expect(json.keys).to be_any
+
+          json.keys.each do |key|
+            expect(key.to_sym.in?(answer_attributes.keys)).to be true
+          end
+        end
+      end
+    end
+
+    context 'as not owner of resource' do
+      let(:answer2) { create(:answer) }
+      let(:answer_attributes) { attributes_for(:answer) }
+
+      before do
+        patch "/api/v1/answers/#{answer2.id}",
+              params: { access_token: access_token.token,
+                        answer: answer_attributes },
+              headers: headers
+      end
+
+      it 'returns 302 status' do
+        expect(response.status).to eq 302
+      end
+    end
+  end
+
+  describe 'DELETE api/v1/answers/:id' do
+    let(:user) { create(:user) }
+    let!(:question) { create(:question) }
+    let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+    let!(:answer) { create(:answer, question: question, user: user) }
+
+    it_behaves_like 'API Authorizable' do
+      let(:api_path) { "/api/v1/answers/#{answer.id}" }
+      let(:method) { :delete }
+    end
+
+    context 'as owner of resource' do
+      it 'returns 200 status' do
+        expect {
+          delete "/api/v1/answers/#{answer.id}",
+                 params: { access_token: access_token.token },
+                 headers: headers
+        }.to change(Answer, :count).by(-1)
+
+        expect(response).to be_successful
+      end
+    end
+
+    context 'as not owner of resource' do
+      let!(:answer2) { create(:answer) }
+
+      before do
+        delete "/api/v1/answers/#{answer2.id}",
+               params: { access_token: access_token.token },
+               headers: headers
+      end
+
+      it 'returns 302 status' do
+        expect {
+          delete "/api/v1/answers/#{answer2.id}",
+                 params: { access_token: access_token.token },
+                 headers: headers
+        }.to change(Answer, :count).by(0)
+
+        expect(response.status).to eq 302
       end
     end
   end
