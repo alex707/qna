93b0f22ae9faecb6393d1ea3015ba1535cb86484
alex707 <alex.grigorenko2942@gmail.com>
2020-02-29 22:38:48 +0300

    checking statuses and bodies for subscriptions controller

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -9,15 +9,18 @@ RSpec.describe SubscriptionsController, type: :controller do
     context 'as authenticated user' do
       before { login(user) }
 
-      it 'creates question subscription for user' do
-        expect {
-          post :create, params: { question_id: question }, format: :js
-        }.to change(user.subscriptions, :count).by(1)
-      end
+      context 'without subscription' do
+        it 'creates question subscription for user' do
+          expect {
+            post :create, params: { question_id: question }, format: :js
+          }.to change(user.subscriptions, :count).by(1)
+        end
 
-      it 'returns http success' do
-        post :create, params: { question_id: question.id }, format: :js
-        expect(response).to have_http_status(:success)
+        it 'returns http success' do
+          post :create, params: { question_id: question.id }, format: :js
+          expect(response).to have_http_status(:success)
+          expect(response.body).to have_content('ok')
+        end
       end
     end
 
@@ -26,26 +29,37 @@ RSpec.describe SubscriptionsController, type: :controller do
         expect {
           post :create, params: { question_id: question }, format: :js
         }.to change(Subscription, :count).by(0)
+        expect(response.body).to have_content('You need to sign')
+      end
+
+      it 'returns unauth status' do
+        post :create, params: { question_id: question }, format: :js
+
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to have_content('You need to sign in')
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    before { login(user) }
-
     context 'as authenticated user' do
+      before { login(user) }
       before { question.subscribe!(user) }
 
-      it 'removes question subscribe for user' do
-        user.subscriptions.reload
-        expect {
-          delete :destroy, params: { question_id: question }, format: :js
-        }.to change(user.subscriptions, :count).by(-1)
-      end
+      context 'with subscription' do
+        it 'removes question subscribe for user' do
+          user.subscriptions.reload
+          expect {
+            delete :destroy, params: { question_id: question }, format: :js
+          }.to change(user.subscriptions, :count).by(-1)
+        end
 
-      it 'returns http success' do
-        delete :destroy, params: { question_id: question }, format: :js
-        expect(response).to have_http_status(:success)
+        it 'returns http success' do
+          delete :destroy, params: { question_id: question }, format: :js
+
+          expect(response).to have_http_status(:success)
+          expect(response.body).to have_content('ok')
+        end
       end
 
       context 'without subscription' do
@@ -69,6 +83,13 @@ RSpec.describe SubscriptionsController, type: :controller do
         expect {
           delete :destroy, params: { question_id: question }, format: :js
         }.to change(Subscription, :count).by(0)
+      end
+
+      it 'returns unauth status' do
+        delete :destroy, params: { question_id: question }, format: :js
+
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to have_content('You need to sign in')
       end
     end
   end
